d38c225afae79185312996b241998a8a4101978c
Marc España Quinquilla <44228378+Marc1515@users.noreply.github.com>
2023-06-13 13:12:30 +0200

    Inyección del servicio creado en el componente search-component.

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'app-search-box',
@@ -11,13 +12,15 @@ export class SearchBoxComponent {
   tagInput!: ElementRef<HTMLInputElement>
 
 
-  constructor() {}
+  constructor( private gifsService: GifsService ) {}
 
   searchTag() {
 
     const newTag = this.tagInput.nativeElement.value;
 
-    console.log({newTag})
+    this.gifsService.searchTag(newTag);
+
+    this.tagInput.nativeElement.value = '';
 
   }
 
