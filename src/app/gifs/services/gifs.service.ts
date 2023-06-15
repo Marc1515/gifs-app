9f9df66440a8ca50cfe882b8cfa92940b12d5cd7
Marc España Quinquilla <44228378+Marc1515@users.noreply.github.com>
2023-06-15 16:22:27 +0200

    Integramos la interfaz en el servicio.

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { Gif, SearchResponse } from '../interfaces/gifs.interface';
 
 
 const GIPHY_API_KEY = 'Er2HU62YEsEMKoepR3GAxvsaKq3Emmlz';
@@ -7,6 +8,8 @@ const GIPHY_API_KEY = 'Er2HU62YEsEMKoepR3GAxvsaKq3Emmlz';
 @Injectable({providedIn: 'root'})
 export class GifsService {
 
+
+  public gifsList: Gif[] = [];
 
   private _tagsHistory: string[] = [];
   private apiKey: string = GIPHY_API_KEY;
@@ -45,7 +48,7 @@ export class GifsService {
       .set('limit', '10')
       .set('q', tag)
 
-    this.http.get(`${ this.serviceUrl }/search`, { params })
+    this.http.get<SearchResponse>(`${ this.serviceUrl }/search`, { params })
       .subscribe(resp => {
         console.log(resp)
       })
