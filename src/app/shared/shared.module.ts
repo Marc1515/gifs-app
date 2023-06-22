c08151903f46f95b76c97c032d948e0edeb322f4
Marc España Quinquilla <44228378+Marc1515@users.noreply.github.com>
2023-06-22 17:36:24 +0200

    BurguerButtonComponent ha sido movido de directorio y se ha tenido que cambiar la ruta.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { LazyImageComponent } from './components/lazy-image/lazy-image.component';
+import { BurguerButtonComponent } from './burguer-button/burguer-button.component';
 
 
 
 @NgModule({
   declarations: [
     SidebarComponent,
-    LazyImageComponent
+    LazyImageComponent,
+    BurguerButtonComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     SidebarComponent,
-    LazyImageComponent
+    LazyImageComponent,
+    BurguerButtonComponent
   ]
 })
 export class SharedModule { }
