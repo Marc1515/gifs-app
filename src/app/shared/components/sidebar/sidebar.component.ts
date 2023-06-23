fa42ca10f014588fbf962f9b9a3f734ee0786458
Marc España Quinquilla <44228378+Marc1515@users.noreply.github.com>
2023-06-23 13:35:04 +0200

    Lógica para llamar al modal implementado en la sidebar.

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { GifsService } from '../../../gifs/services/gifs.service';
 import { SharedSidebarService } from './shared-sidebar.service';
+import { MatDialog } from '@angular/material/dialog';
+import { SidebarConfirmationDialogComponent } from './components/sidebar-confirmation-dialog/sidebar-confirmation-dialog.component';
 
 @Component({
   selector: 'shared-sidebar',
@@ -12,7 +14,7 @@ export class SidebarComponent {
   public isSidebarVisible: boolean = true;
   public isOpen: boolean = false;
 
-  constructor(private gifsService:GifsService, private sharedSidebarService: SharedSidebarService) {
+  constructor(private gifsService:GifsService, private sharedSidebarService: SharedSidebarService, public dialog: MatDialog) {
 
     this.sharedSidebarService.sidebarToggled.subscribe((isOpen: boolean) => {
       this.isSidebarVisible = !isOpen;
@@ -31,6 +33,19 @@ export class SidebarComponent {
       this.sharedSidebarService.tagClicked.emit(tag);  // Ocultar la barra lateral
     }
   }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(SidebarConfirmationDialogComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.resetTag()
+      } else {
+        // El usuario hizo clic en "No" o cerró el cuadro de diálogo.
+      }
+    });
+  }
+
 
   resetTag() {
     this.gifsService.resetTags();
